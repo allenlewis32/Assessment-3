bb5449726ba56ae1f666786333108d8413258498
Allen Lewis <allenlewis32@gmail.com>
2023-04-07 11:23:58 +0530

    Initial commit

---
+++ Assessment-3/TournamentManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Assessment_3
+{
+    internal class TournamentManager
+    {
+        SqlConnection sqlConnection;
+        const string connectionString = "Data Source=DESKTOP-IQRSRP8;Initial Catalog=sport;Integrated Security=True;Encrypt=False;";
+        static void Main(string[] args)
+        {
+            TournamentManager manager = new();
+        }
+        public TournamentManager()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+        }
+    }
+}
